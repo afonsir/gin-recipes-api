e9c1a70eb49b3829eb80aef5dd6433ce2264266a
Afonso Costa <afonsir17@gmail.com>
2022-02-14 21:21:23 -0300

    #5 [FEAT] Delete Recipe (DELETE /recipes)

--- main.go
+++ main.go
@@ -33,6 +33,7 @@ func main() {
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipesHandler)
+	router.DELETE("/recipes/:id", DeleteRecipesHandler)
 
 	router.Run()
 }
@@ -88,4 +89,28 @@ func UpdateRecipesHandler(c *gin.Context) {
 	recipes[index] = recipe
 
 	c.JSON(http.StatusOK, recipe)
+}
+
+func DeleteRecipesHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted",
+	})
 }
