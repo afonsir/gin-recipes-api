144db967c6517849a992273c71d872c7b78f380e
AfonSir <afonsir17@gmail.com>
2022-02-19 18:19:07 -0300

    feat: connect to mongodb

--- main.go
+++ main.go
@@ -18,31 +18,51 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
+	"context"
+	"fmt"
+	"log"
 	"net/http"
-	"strings"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/rs/xid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+// swagger:parameters recipes newRecipe
 type Recipe struct {
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
+	// swagger:ignore
+	ID           primitive.ObjectID `json:"id"           bson:"_id"`
+	Name         string             `json:"name"         bson:"name"`
+	Tags         []string           `json:"tags"         bson:"tags"`
+	Ingredients  []string           `json:"ingredients"  bson:"ingredients"`
+	Instructions []string           `json:"instructions" bson:"instructions"`
+	PublishedAt  time.Time          `json:"publishedAt"  bson:"publishedAt"`
 }
 
 var recipes []Recipe
+var ctx context.Context
+var err error
+var client *mongo.Client
+var collection *mongo.Collection
 
 func init() {
-	recipes = make([]Recipe, 0)
-	file, _ := ioutil.ReadFile("recipes.json")
-	_ = json.Unmarshal([]byte(file), &recipes)
+	ctx = context.Background()
+
+	client, err = mongo.Connect(ctx,
+		options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+
+	if err = client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to MongoDB")
+
+	collection = client.Database(os.Getenv("MONGODB_DATABASE")).Collection("recipes")
 }
 
 func main() {
